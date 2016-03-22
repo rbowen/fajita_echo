ec8e59250ed86bf7242982dd7d041c02f065af46
Rich Bowen <rbowen@rcbowen.com>
2016-03-22 07:54:38 -0400

    Now we can edit menu items

--- echo.php
+++ echo.php
@@ -5,7 +5,7 @@ $query = json_decode( $data );
 # error_log( print_r( $query, 1 ) );
 
 $me = array(
-    'version' => '0.1',
+    'version' => '0.2',
     'name'    => 'Fajita'
 );
 
@@ -13,19 +13,11 @@ $guid = '179cac3e-f062-48e6-ba41-109a728d01fe';
 $userid = 'AERVD6MDLOMTUHVRFQ2JMWVPXZSHPHCNYUCOEEXB43QOFQL22M36Q';
 $help = "Hi. .  I'm Fajita.  .  Ask me what's for dinner.";
 
-$verb = array(
-    'Have',
-    'How about',
-    'Try',
-    'What about',
-    'You could have',
-);
-
 try {
     $db = new PDO('mysql:host=localhost;dbname=fajita;charset=utf8mb4', 'fajita', 'fajita');
 }
 catch(PDOException $e) {
-    error_log( $e->getMessage() ) ;
+    error_log( $e->getMessage() );
 }
 
 include('../validate-echo-request-php/valid_request.php');
@@ -47,31 +39,17 @@ if ( $query ) {
 
     # Get a suggestion for dinner
     if ( $action == 'GetVegetarian' || $action == 'GetMenu' ) {
-
-        $dbq = "SELECT name FROM menu ";
-        if ( $vegetarian ) {
-            $dbq = $dbq . " WHERE vegetarian IS TRUE ";
-        }
-        $dbq = $dbq . " ORDER BY RAND() LIMIT 0,1 ";
-
-        $sth = $db->prepare( $dbq );
-        $sth->execute();
-        $result = $sth->fetch();
-        $meal = $result['name'];
-
-        $recommend = $verb[ array_rand( $verb )] . ' '
-                   . $meal . ' for dinner';
-
-        $response = $recommend;
+        $response = getmenu( $db, $vegetarian );
     }
 
     # Add an item to the menu
     elseif ( $action == 'AddMenu' ) {
+        $response = addmenu( $db, $query );
     }
 
     # Help
     elseif ( $action == 'AMAZON.HelpIntent' ) {
-        sendresponse( $help, $me );
+        $response = $help;
     }
 
 }
@@ -82,6 +60,60 @@ else {
 
 sendresponse( $response, $me );
 echo json_encode($response);
+
+/*
+    Get a menu recommendation
+
+    getmenu( $db, $vegetarian );
+*/
+function getmenu( $db, $vegetarian = 0 ) {
+
+    # various ways to say it
+    $verb = array(
+        'Have',
+        'How about',
+        'Try',
+        'What about',
+        'You could have',
+    );
+
+    $dbq = "SELECT name FROM menu ";
+
+    # Vegetarian?
+    if ( $vegetarian ) {
+        $dbq = $dbq . " WHERE vegetarian IS TRUE ";
+    }
+
+    # Random
+    $dbq = $dbq . " ORDER BY RAND() LIMIT 0,1 ";
+
+    $sth = $db->prepare( $dbq );
+    $sth->execute();
+    $result = $sth->fetch();
+    $meal = $result['name'];
+
+    $recommend = $verb[ array_rand( $verb )] . ' '
+               . $meal . ' for dinner';
+
+    return( $recommend );
+}
+
+/*
+    Add an item to the menu
+
+    addmenu( $db, $query );
+*/
+function addmenu( $db, $query ) {
+    $menu = $query->request->intent->slots->Menu->value;
+
+    $sth = $db->prepare( "INSERT INTO menu (name)
+        values ( ? ) ");
+    $sth->execute( array( $menu ) );
+    $insertid = $db->lastInsertId();
+
+    $response = 'I added ' . $menu . ' to your menu options.';
+    return( $response );
+}
 
 ?>
 
